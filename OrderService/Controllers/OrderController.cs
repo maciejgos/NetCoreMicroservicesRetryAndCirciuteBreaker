90ff2380e0c2058691fdd8621f1ede344bef89e7
Maciej Gos <maciej@maciejgos.com>
2020-06-30 11:39:20 +0200

    Order controller implementation

---
+++ OrderService/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderService.Services;
+using System.Threading.Tasks;
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+
+        public OrderController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        public async Task<IActionResult> Post([FromBody] OrderDto order)
+        {
+            // Some logic to place an order
+            var response = await _paymentService.MakePayment(new PaymentRequest { Amount = 100.25 });
+            if (response.Status == "Success")
+                return new OkResult();
+            else
+                return new OkObjectResult("Unable to process payment");
+        }
+    }
+
+    public class OrderDto
+    {
+        public string Name { get; set; }
+    }
+}
--- OrderService/Startup.cs
+++ OrderService/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using OrderService.Services;
+using System;
 
 namespace OrderService
 {
@@ -26,6 +21,7 @@ namespace OrderService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddHttpClient<IPaymentService, PaymentService>(client => client.BaseAddress = new Uri(Configuration["PaymentServiceUrl"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
