7bf7c7b01dc9c81ffb0892a3c8311923c0c9857e
Maciej Gos <maciej@maciejgos.com>
2020-06-30 11:44:38 +0200

    Payment service implementation

---
+++ PaymentService/Controllers/PaymentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        public IActionResult Post([FromBody] PaymentRequest request)
+        {
+            return new OkResult();
+        }
+
+        public class PaymentRequest
+        {
+            public double Amount { get; set; }
+        }
+    }
+}
